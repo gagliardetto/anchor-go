b45b3ca8a857c6e8cc214297f8f1e164a4842c45
Slavomir <gagliardetto@users.noreply.github.com>
2021-09-21 17:44:41 +0200

    Upgrade `github.com/gagliardetto/binary` to `v0.4.4`; upgrade `github.com/gagliardetto/solana-go` to `v0.5.1`.

--- main.go
+++ main.go
@@ -149,8 +149,8 @@ func main() {
 			mdf := &modfile.File{}
 			mdf.AddModuleStmt(GetConfig().ModPath)
 
-			mdf.AddNewRequire("github.com/gagliardetto/solana-go", "v0.5.0", false)
-			mdf.AddNewRequire("github.com/gagliardetto/binary", "v0.4.3", false)
+			mdf.AddNewRequire("github.com/gagliardetto/solana-go", "v0.5.1", false)
+			mdf.AddNewRequire("github.com/gagliardetto/binary", "v0.4.4", false)
 			mdf.AddNewRequire("github.com/gagliardetto/treeout", "v0.1.4", false)
 			mdf.AddNewRequire("github.com/gagliardetto/gofuzz", "v1.2.2", false)
 			mdf.AddNewRequire("github.com/stretchr/testify", "v1.6.1", false)
